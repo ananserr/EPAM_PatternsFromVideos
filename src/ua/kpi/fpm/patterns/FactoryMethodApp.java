a7df494bd47574787f8e12c74db105dd1ffb54bf
Anastasia Serhienko <ananserr@gmail.com>
2016-06-29 12:42:25 +0300

    Factory method pattern

---
+++ src/ua/kpi/fpm/patterns/FactoryMethodApp.java
@@ -0,0 +1,62 @@
+package ua.kpi.fpm.patterns;
+
+import java.util.Date;
+
+/**
+ * Created by Anastasia Serhienko on 6/20/16.
+ */
+public class FactoryMethodApp {
+    public static void main(String[] args) {
+        WatchMaker maker = getMakerByName("Digital");
+        Watch watch = maker.createWatch();
+        watch.showTime();
+    }
+    public static WatchMaker getMakerByName(String maker) {
+        if (maker.equals("Digital"))
+            return new DigitalWatchMaker();
+        else if (maker.equals("Rome"))
+            return new RomeWatchMaker();
+
+        throw new RuntimeException("Не поддерживаемая производственная линия часов: " + maker);
+    }
+}
+
+interface Watch {
+    void showTime();
+}
+
+class DigitalWatch implements Watch {
+
+    @Override
+    public void showTime() {
+        System.out.println(new Date());
+    }
+}
+
+class RomeWatch implements Watch {
+
+    @Override
+    public void showTime() {
+        System.out.println("VII-XX");
+    }
+}
+
+interface WatchMaker {
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker {
+
+    @Override
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
+
+class RomeWatchMaker implements WatchMaker {
+
+    @Override
+    public Watch createWatch() {
+        return new RomeWatch();
+    }
+}
